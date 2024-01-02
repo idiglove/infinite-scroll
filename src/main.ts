21c95bee2535648a5f899f8d9db9cf66dd09d63c
Faith Morante <faith.morante@gmail.com>
2024-01-02 12:41:37 -0800

    simulate infinite scroll

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
-import Card from "./Card/index";
-
 function main() {
-  // const element = document.getElementById("app");
-  // const card = Card();
-  // if (element) {
-  //   element.innerHTML = card;
-  // }
+  const $list = document.querySelector(".list")!;
+  // Simulate a request to load data and render it to the list element;
+  function loadItems(number: number) {
+    new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(Array(number).fill(null));
+      }, 1000);
+    }).then((data: any) => {
+      data.forEach(() => {
+        const item = document.querySelector("card-item")?.cloneNode(true)!;
+        $list.append(item);
+      });
+    });
+  }
+
+  const intersectionObserver = new IntersectionObserver((entries) => {
+    if (entries[0].intersectionRatio <= 0) return;
+    loadItems(10);
+  });
+  // start observing
+  intersectionObserver.observe(document.querySelector(".more")!);
 }
 document.addEventListener("DOMContentLoaded", main);
